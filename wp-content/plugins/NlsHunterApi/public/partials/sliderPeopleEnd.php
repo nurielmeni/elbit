3b12eec09ccc050460b1f45ccce020241778eb11
Meni Nuriel <nurielmeni@gmail.com>
2021-12-13 21:55:25 +0200

    Typo

--- wp-content/plugins/NlsHunterApi/public/partials/sliderPeopleEnd.php
+++ wp-content/plugins/NlsHunterApi/public/partials/sliderPeopleEnd.php
@@ -1,6 +1,6 @@
         <div class="dots">
-            <span class="btn-dots next"><i tabindex="0" role="button" aroa-label="הצג אנשים הבאים" class="fa fa-angle-right" aria-hidden="true"></i></span>
-            <span class="btn-dots prev"><i tabindex="0" role="button" aroa-label="הצג אנשים קודמים" class="fa fa-angle-left" aria-hidden="true"></i></span>
+            <span class="btn-dots next"><i tabindex="0" role="button" aria-label="הצג אנשים הבאים" class="fa fa-angle-right" aria-hidden="true"></i></span>
+            <span class="btn-dots prev"><i tabindex="0" role="button" aria-label="הצג אנשים קודמים" class="fa fa-angle-left" aria-hidden="true"></i></span>
         </div>
-    </div>
-</div>
+        </div>
+        </div>
